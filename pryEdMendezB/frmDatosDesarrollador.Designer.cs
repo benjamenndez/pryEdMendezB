a535f279f4bdb51155a5065f56d02e2ff5c6447c
Alumno <Alumno@PC51>
2026-03-26 10:18:26 -0300

    Agregar archivos de proyecto.

---
+++ pryEdMendezB/frmDatosDesarrollador.Designer.cs
@@ -0,0 +1,81 @@
+﻿namespace pryEdMendezB
+{
+    partial class frmDatosDesarrollador
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblDni = new System.Windows.Forms.Label();
+            this.lblEstructuraDatos = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // lblDni
+            // 
+            this.lblDni.AutoSize = true;
+            this.lblDni.BackColor = System.Drawing.Color.Sienna;
+            this.lblDni.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.lblDni.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblDni.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.lblDni.Location = new System.Drawing.Point(333, 9);
+            this.lblDni.Name = "lblDni";
+            this.lblDni.Size = new System.Drawing.Size(82, 20);
+            this.lblDni.TabIndex = 0;
+            this.lblDni.Text = "47351653";
+            // 
+            // lblEstructuraDatos
+            // 
+            this.lblEstructuraDatos.AutoSize = true;
+            this.lblEstructuraDatos.BackColor = System.Drawing.Color.Sienna;
+            this.lblEstructuraDatos.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblEstructuraDatos.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.lblEstructuraDatos.Location = new System.Drawing.Point(303, 29);
+            this.lblEstructuraDatos.Name = "lblEstructuraDatos";
+            this.lblEstructuraDatos.Size = new System.Drawing.Size(143, 16);
+            this.lblEstructuraDatos.TabIndex = 1;
+            this.lblEstructuraDatos.Text = "Estructura de Datos";
+            // 
+            // frmDatosDesarrollador
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Linen;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.lblEstructuraDatos);
+            this.Controls.Add(this.lblDni);
+            this.Name = "frmDatosDesarrollador";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "frmDatosDesarrollador";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblDni;
+        private System.Windows.Forms.Label lblEstructuraDatos;
+    }
+}
---
+++ pryEdMendezB/frmMeses.Designer.cs
@@ -0,0 +1,108 @@
+﻿namespace pryEdMendezB
+{
+    partial class frmMeses
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblNombre = new System.Windows.Forms.Label();
+            this.txtNombre = new System.Windows.Forms.TextBox();
+            this.btnListar = new System.Windows.Forms.Button();
+            this.btnGrabar = new System.Windows.Forms.Button();
+            this.lstMeses = new System.Windows.Forms.ListBox();
+            this.SuspendLayout();
+            // 
+            // lblNombre
+            // 
+            this.lblNombre.AutoSize = true;
+            this.lblNombre.Location = new System.Drawing.Point(35, 24);
+            this.lblNombre.Name = "lblNombre";
+            this.lblNombre.Size = new System.Drawing.Size(47, 13);
+            this.lblNombre.TabIndex = 0;
+            this.lblNombre.Text = "Nombre:";
+            // 
+            // txtNombre
+            // 
+            this.txtNombre.Location = new System.Drawing.Point(107, 21);
+            this.txtNombre.Name = "txtNombre";
+            this.txtNombre.Size = new System.Drawing.Size(142, 20);
+            this.txtNombre.TabIndex = 1;
+            // 
+            // btnListar
+            // 
+            this.btnListar.Location = new System.Drawing.Point(150, 47);
+            this.btnListar.Name = "btnListar";
+            this.btnListar.Size = new System.Drawing.Size(99, 23);
+            this.btnListar.TabIndex = 2;
+            this.btnListar.Text = "Listar";
+            this.btnListar.UseVisualStyleBackColor = true;
+            this.btnListar.Click += new System.EventHandler(this.btnListar_Click);
+            // 
+            // btnGrabar
+            // 
+            this.btnGrabar.Location = new System.Drawing.Point(38, 47);
+            this.btnGrabar.Name = "btnGrabar";
+            this.btnGrabar.Size = new System.Drawing.Size(106, 23);
+            this.btnGrabar.TabIndex = 3;
+            this.btnGrabar.Text = "Grabar";
+            this.btnGrabar.UseVisualStyleBackColor = true;
+            // 
+            // lstMeses
+            // 
+            this.lstMeses.FormattingEnabled = true;
+            this.lstMeses.Location = new System.Drawing.Point(38, 76);
+            this.lstMeses.Name = "lstMeses";
+            this.lstMeses.Size = new System.Drawing.Size(211, 199);
+            this.lstMeses.TabIndex = 4;
+            // 
+            // frmMeses
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Linen;
+            this.ClientSize = new System.Drawing.Size(292, 313);
+            this.Controls.Add(this.lstMeses);
+            this.Controls.Add(this.btnGrabar);
+            this.Controls.Add(this.btnListar);
+            this.Controls.Add(this.txtNombre);
+            this.Controls.Add(this.lblNombre);
+            this.Name = "frmMeses";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "frmMeses";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblNombre;
+        private System.Windows.Forms.TextBox txtNombre;
+        private System.Windows.Forms.Button btnListar;
+        private System.Windows.Forms.Button btnGrabar;
+        private System.Windows.Forms.ListBox lstMeses;
+    }
+}
