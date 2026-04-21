60381a8ea6e32b41ba203bd00a191ea163379de7
benjamenndez <benjamendez717@gmail.com>
2026-04-21 20:19:50 -0300

    21/04 agregue clase clsCola y sume codigo

---
+++ pryEdMendezB/clsCola.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace pryEdMendezB
+{
+    internal class clsCola
+    {
+        //campos de las clase
+        private clsNodo pri;
+        private clsNodo ult;
+
+        //propiedades de la clase
+        public clsNodo Primero
+        {
+            get { return pri; }
+            set { pri = value; }
+        }
+        public clsNodo Ultimo
+        {
+            get { return ult; }
+            set { ult = value; }
+        }
+
+        //metodos de la clase
+        public void Agregar(clsNodo Nuevo)
+        {
+            if (Primero == null)
+            {
+                Primero = Nuevo;
+                Ultimo = Nuevo;
+            }
+            else
+            {
+                Ultimo.Siguiente = Nuevo;
+                Ultimo = Nuevo;
+            }
+        }
+        public void Eliminar()
+        {
+            if (Primero == Ultimo)
+            {
+
+                Primero = null;
+                Ultimo = null;
+               
+            }
+            else
+            {
+                Primero = Primero.Siguiente;
+            }
+        }
+        public void Recorrer(DataGridView Grilla)
+        {
+            clsNodo Aux = Primero;
+            Grilla.Rows.Clear();
+            while ( Aux != null)
+            {
+                Grilla.Rows.Add(Aux.Codigo, Aux.Nombre, Aux.Tramite);
+                Aux = Aux.Siguiente;
+            }
+        }
+        public void Recorrer(ListBox Lista)
+        {
+            clsNodo Aux = Primero;
+            Lista.Items.Clear();
+            while (Aux != null)
+            {
+                Lista.Items.Add(Aux.Codigo);
+                Aux = Aux.Siguiente;
+            }
+        }
+        public void Recorrer(ComboBox Combo)
+        {
+            clsNodo Aux = Primero;
+            Combo.Items.Clear();
+            while (Aux != null)
+            {
+                Combo.Items.Add(Aux.Nombre);
+                Aux = Aux.Siguiente;
+            }
+        }
+    }
+    
+}   
